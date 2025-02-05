c86bd69d9b6aee66f275f1a0268890724d1a38b6
BugHunter-X <askzenai@gmail.com>
2025-02-05 17:30:05 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,17 @@
+function result = myFunction(x)
+  if x > 10
+    result = x^2; 
+  elseif x < 0
+    result = 0; 
+  else
+    result = x + 1;
+  end
+end
+
+%Example usage
+>> myFunction(5)
+ans = 6
+>> myFunction(12)
+ans = 144
+>> myFunction(-3)
+ans = 0
